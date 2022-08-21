32aeb05efa2a9535bea42362daf6bf064f5d7efd
Felipe M. Vieira <felipev@telnyx.com>
2022-08-21 10:48:38 -0300

    added tests to `example.erl`

--- fmv1992_database_client/test/eunit_tests.erl
+++ fmv1992_database_client/test/eunit_tests.erl
@@ -6,9 +6,28 @@
 
 reverse_test() -> lists:reverse([1, 2, 3]).
 
+check_that_the_db_is_working() ->
+    {ok, C} = epgsql:connect(#{
+        host => "localhost",
+        username => "fmv1992_database_user",
+        password => "password_fmv1992_database_postgres",
+        database => "fmv1992_database",
+        port => 5999,
+        timeout => 4000
+    }),
+    {ok, [Column], Data} = epgsql:equery(C, "SELECT 1::int as mycol;"),
+    ?assertEqual([{1}], Data),
+    ?assertEqual(column, erlang:element(1, Column)),
+    ?assertEqual(<<"mycol">>, erlang:element(2, Column)).
+
 poolboy_test() ->
+    check_that_the_db_is_working(),
+    % Start the application.
+    Pid1 = example:start(),
+    % Start the supervisor. This will also call `init'.
+    Pid2 = example:start([], []),
+    {ok, [_Column], Data} = example:equery(pool1, "SELECT 'POOLBOY!' as x;", []),
     ?assertEqual(
-        1,
-        % fmv1992_database_client:main()
-        example:main()
+        [{<<"POOLBOY!">>}],
+        Data
     ).
